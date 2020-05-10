e3852b55d305ce9b7a5162d07d6db4f593f250c0
kmarks2013 <kmarks2013@gmail.com>
2020-05-10 00:28:38 -0400

    turns card back in to empty div and css

--- src/Components/CardBack.js
+++ src/Components/CardBack.js
@@ -1,29 +1,10 @@
 import React, { Component } from 'react'
 
 export default class CardBack extends Component {
-    state = {
-        cardFace: false
-    }
 
-    handleOnClick = () => {
-        this.setState({
-            cardFace: !this.state.cardFace
-        })
-    }
     render() {
         return (
-            <>
-            {
-                this.state.cardFace ? 
-                <div className="dealer-second-card" onClick={this.handleOnClick} >
-                   <img src={this.props.card.image} />
-                </div> : 
-                  <div className="dealer-second-card card-back" onClick={this.handleOnClick} >
-                  <img src={this.props.card.image} />
-              </div> 
-            }
-           
-           </>
+            <div className="card-back" onClick={this.props.handleOnClick}></div>
         )
     }
 }
